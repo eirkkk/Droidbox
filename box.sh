e3d4f07811e9e83d8b9f7ec6e8449d21f6d764fc
eirkkk <33490753+eirkkk@users.noreply.github.com>
2023-09-19 09:08:29 +0300

    Update box.sh

--- box.sh
+++ box.sh
@@ -4,39 +4,21 @@
 apt update
 
 # Install dependencies
-apt install -y git build-essential cmake wget gcc-arm-linux-gnueabihf zenity:armhf libasound*:armhf libstdc++6:armhf 
 apt install -y nano cabextract libfreetype6 libfreetype6:armhf libfontconfig libfontconfig:armhf libxext6 libxext6:armhf libxinerama-dev libxinerama-dev:armhf libxxf86vm1 libxxf86vm1:armhf libxrender1 libxrender1:armhf libxcomposite1 libxcomposite1:armhf libxrandr2 libxrandr2:armhf libxi6 libxi6:armhf libxcursor1 libxcursor1:armhf libvulkan-dev libvulkan-dev:armhf
+apt install  -y libc6:armhf gpg
 
 # Clone Box86 repository
-git clone https://github.com/ptitSeb/box86
-
-# Build and install Box86
-cd box86
-mkdir build && cd build
-cmake .. -DARM64=1 -DARM_DYNAREC=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBAD_SIGNAL=ON -DSD845=ON 
-make -j8
-make install
-
-# Clean up Box86 repository
-cd /root
-rm -r box86
-
-echo "Box86 installation completed."
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
+wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+apt update
+apt install box86-android:armhf -y
 
 # Clone Box64 repository
-git clone https://github.com/ptitSeb/box64
-
-# Build and install Box64
-cd box64
-mkdir build && cd build
-cmake .. -DARM_DYNAREC=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBAD_SIGNAL=ON -DSD845=ON
-make -j8
-make install
-
-# Clean up Box64 repository
-cd /root
-rm -r box64
-
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
+wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+apt update 
+apt install box64-android -y
+  
 echo "Box64 installation completed."
 
 # Download and install Wine Proton
