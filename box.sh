d294862176f9bbc36f8b47001b3e70de9454cb88
eirkkk <33490753+eirkkk@users.noreply.github.com>
2023-09-20 10:45:12 +0300

    Update box.sh

--- box.sh
+++ box.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+echo "
+deb http://ports.ubuntu.com/ubuntu-ports mantic main restricted universe multiverse
+deb-src http://ports.ubuntu.com/ubuntu-ports mantic main restricted universe multiverse
 
+deb http://ports.ubuntu.com/ubuntu-ports mantic-updates main restricted universe multiverse
+deb-src http://ports.ubuntu.com/ubuntu-ports jammy-updates main restricted universe multiverse
+
+deb http://ports.ubuntu.com/ubuntu-ports mantic-backports main restricted universe multiverse
+deb-src http://ports.ubuntu.com/ubuntu-ports mantic-backports main restricted universe multiverse
+
+deb http://ports.ubuntu.com/ubuntu-ports mantic-security main restricted universe multiverse
+deb-src http://ports.ubuntu.com/ubuntu-ports mantic-security main restricted universe multiverse
+" > /etc/apt/sources.list
+
+dpkg --add-architecture armhf
 # Update package manager
 apt update
 
