7214217fc86062310eadfe8e67e728630ea78fe8
eirkkk <33490753+eirkkk@users.noreply.github.com>
2023-10-04 19:22:13 +0300

    Update build_deb_mesa.sh

--- build_deb_mesa.sh
+++ build_deb_mesa.sh
@@ -64,8 +64,8 @@ endian = 'little'
 
 # Build mesa 
 cd ${MESA_PREFIX}
-wget https://raw.githubusercontent.com/eirkkk/Mesa_turnip_termux/main/wsi_common_x11.c
-wget https://raw.githubusercontent.com/eirkkk/Mesa_turnip_termux/main/wsi-termux-x11-v3.patch
+wget https://raw.githubusercontent.com/eirkkk/Droidbox/main/wsi_common_x11.c
+wget https://raw.githubusercontent.com/eirkkk/Droidbox/main/wsi-termux-x11-v3.patch
 git apply -v wsi-termux-x11-v3.patch
 rm -rf src/vulkan/wsi/wsi_common_x11.c
 cp -r wsi_common_x11.c src/vulkan/wsi/
