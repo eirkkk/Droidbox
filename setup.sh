78cbb280968aa3381ac4921a811ff5ca3cfebf87
eirkkk <33490753+eirkkk@users.noreply.github.com>
2023-10-05 07:07:40 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,9 +18,8 @@ start_time=$(date +%s)
 echo "Executing... Please wait."
 
 # Execute the commands
-pkg up 
+pkg up && pkg upgrade
 sleep 5
-pkg upgrade 
 clear
 # Display a waiting message
 echo "Executing... Please wait."
