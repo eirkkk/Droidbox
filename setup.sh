8d0fa3d3d606b4c5f91fe0768e95522be913dd2f
eirkkk <33490753+eirkkk@users.noreply.github.com>
2024-02-13 04:18:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,12 +26,14 @@ echo "Executing... Please wait."
 pkg install x11-repo -y > /dev/null 2>&1
 pkg install proot-distro proot termux-x11-nightly wget git pulseaudio -y > /dev/null 2>&1
 termux-setup-storage > /dev/null 2>&1
-proot-distro install debian > /dev/null 2>&1
+wget  https://cdimage.ubuntu.com/ubuntu-base/daily/current/noble-base-arm64.tar.gz > /dev/null 2>&1
 wget https://raw.githubusercontent.com/eirkkk/Droidbox/main/start-droidbox > /dev/null 2>&1
 wget https://raw.githubusercontent.com/eirkkk/Droidbox/main/droidbox > /dev/null 2>&1
 chmod +x start-droidbox && chmod +x droidbox > /dev/null 2>&1
 cp -r droidbox start-droidbox /data/data/com.termux/files/usr/bin > /dev/null 2>&1
 rm -rf droidbox start-droidbox > /dev/null 2>&1
+mkdir $HOME/.ubuntu
+proot --link2symlink tar -xf noble-base-arm64.tar.gz -C $HOME/.ubuntu
 
 # Countdown timer synchronized with execution
 while [ $countdown -gt 0 ]; do
