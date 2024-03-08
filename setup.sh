cccec277846d35e345f65b5891241ab9731a018d
eirkkk <33490753+eirkkk@users.noreply.github.com>
2024-03-08 07:28:30 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ termux-setup-storage
 
 # Download Ubuntu base image
 echo "Downloading Rootfs base image..."
-wget "https://github.com/debuerreotype/docker-debian-artifacts/blob/dist-arm64v8/trixie/rootfs.tar.xz?raw=true" -O rootfs.tar.xz   > /dev/null 2>&1
+wget https://github.com/eirkkk/Droidbox/releases/download/Eirkkk/rootfs.tar.xz > /dev/null 2>&1
 clear
 # Depending on user choice, download and configure the appropriate version
 if [ $version_choice -eq 1 ]; then
@@ -47,22 +47,17 @@ if [ $version_choice -eq 1 ]; then
     wget -q https://raw.githubusercontent.com/eirkkk/Droidbox/main/Droidbox/droidbox  > /dev/null 2>&1
     chmod +x droidbox
     mv droidbox /data/data/com.termux/files/usr/bin/
+   sudo proot --link2symlink tar -xf rootfs.tar.xz -C /data/local/ > /dev/null 2>&1
+   sudo echo 'nameserver 1.1.1.1' >> /data/local/.debian/etc/resolv.conf
+   
 elif [ $version_choice -eq 2 ]; then
-    echo "Downloading and configuring Proot version..."
+    echo "Downloading and configurg Proot version..."
     wget -q https://raw.githubusercontent.com/eirkkk/Droidbox/main/start-droidbox  > /dev/null 2>&1
     wget -q https://raw.githubusercontent.com/eirkkk/Droidbox/main/droidbox  > /dev/null 2>&1
     chmod +x start-droidbox droidbox
     mv start-droidbox droidbox /data/data/com.termux/files/usr/bin/
+    proot --link2symlink tar -xf rootfs.tar.xz -C $HOME/.debian > /dev/null 2>&1
 fi
-
-# Create a directory for Ubuntu
-mkdir -p $HOME/.debian
-mkdir $HOME/.debian/sdcard
-
-# Extract the Ubuntu base image
-echo "Extracting Rootfs..."
-proot --link2symlink tar -xf rootfs.tar.xz -C $HOME/.debian > /dev/null 2>&1
-echo 'nameserver 1.1.1.1' >> $HOME/.debian/etc/resolv.conf
 # Remove the downloaded tar file
 rm rootfs.tar.xz
 
